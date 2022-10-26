fe9b76785855746d9bf3a796b707a6e08586efbf
Colleen Stewart <collstewart30@gmail.com>
2022-10-26 14:53:47 -0400

    Started logic for POST in comments views.py

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.get_all_comments),
-    path('all/', views.user_comments),
+    path('all/', views.get_all_comments),
+    path('', views.user_comments),
 ]
--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,18 +1,32 @@
-from rest_framework.decorators import api_view
+from os import stat
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework import status
+from .models import Comment
+from .serializers import CommentSerializer
 
 
 
 
-@api_view(['GET'])
+@api_view(['GET'])  # get all comments in database
 @permission_classes([AllowAny])
 def get_all_comments(request):
-    return Response('this is all comments')
+    comments = Comment.objects.all()
+    serializer = CommentSerializer(comments, many=True)
+    return Response(serializer.data)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])      # get by id, post (by id) both authenticated (take id)
 @permission_classes([IsAuthenticated])
 def user_comments(request):
-    return Response('this is the user comment')
+    
+    if request.method == 'GET':
+        comments = Comment.objects.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response('this is the user comment')
+    elif request.method == 'POST':
+        serializer = CommentSerializer(data=request.data)        
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
