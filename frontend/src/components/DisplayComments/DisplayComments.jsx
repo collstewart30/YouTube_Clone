dfdffb70ee701a320203df3aadeb70fb8b6766b4
Colleen Stewart <collstewart30@gmail.com>
2023-01-17 16:45:19 -0500

    Fixed functionality for DisplayComments

--- frontend/src/components/DisplayComments/DisplayComments.jsx
+++ frontend/src/components/DisplayComments/DisplayComments.jsx
@@ -1,20 +1,20 @@
 const DisplayComments = (props) => {
-  // destructuring data: ({songs}) instead of props
 
   return (
     <table className="table">
       <thead>
         <tr>
           <th className="text-muted" style={{ margin: "1em" }}>COMMENTS</th>
+          <th className="text-muted" style={{ margin: "1em" }}>USER</th>
         </tr>
       </thead>
       <tbody>
         {props.parentDisplayComments.map((comment, index) => {
-          // destructuring data: {songs.map((song) =>....rest of table)} then don't need "props."
           return (
             <tr>
               {/* <td>{index + 1}</td> */}
               <td>{comment.text}</td>
+              <td>{comment.user_id}</td>
             </tr>
           );
         })}
--- frontend/src/pages/VideoPage/VideoPage.js
+++ frontend/src/pages/VideoPage/VideoPage.js
@@ -24,6 +24,7 @@ const VideoPage = () => {
 
   useEffect(() => {
     getRelatedVideos();
+    getAllComments();
   }, [videoId]);
 
   const getRelatedVideos = async () => {
